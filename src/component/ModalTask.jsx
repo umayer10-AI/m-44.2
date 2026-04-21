db8ca179536ddd545f138c3aa68af5da4014df09
umayer10-AI <mdumayer10@gmail.com>
2026-04-21 09:31:16 +0600

    add

--- src/component/ModalTask.jsx
+++ src/component/ModalTask.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import {Envelope} from "@gravity-ui/icons";
-import {Button, Input, Label, Modal, Surface, TextField} from "@heroui/react";
+import {Button, Input, Label, Modal, Surface, TextField, ListBox, Select} from "@heroui/react";
 
 const ModalTask = ({createTask}) => {
     return (
@@ -24,21 +24,65 @@ const ModalTask = ({createTask}) => {
                     <Label>Title</Label>
                     <Input placeholder="Enter your cart Title" />
                   </TextField>
-                  <TextField className="w-full" name="email" type="text">
-                    <Label>Email</Label>
-                    <Input placeholder="Enter your email" />
+                  <TextField className="w-full" name="description" type="text">
+                    <Label>Description</Label>
+                    <Input placeholder="Enter your description" />
                   </TextField>
-                  <TextField className="w-full" name="phone" type="tel">
-                    <Label>Phone</Label>
-                    <Input placeholder="Enter your phone number" />
-                  </TextField>
-                  <TextField className="w-full" name="company">
-                    <Label>Company</Label>
-                    <Input placeholder="Enter your company name" />
-                  </TextField>
-                  <TextField className="w-full" name="message">
-                    <Label>Message</Label>
-                    <Input placeholder="Enter your message" />
+
+        <Select className="w-[256px]" name='Priority' placeholder="Select one">
+      <Label>Priority</Label>
+      <Select.Trigger>
+        <Select.Value />
+        <Select.Indicator />
+      </Select.Trigger>
+      <Select.Popover>
+        <ListBox>
+          <ListBox.Item id="low" textValue="Low">
+            Low
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+          <ListBox.Item id="medium" textValue="Medium">
+            Medium
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+          <ListBox.Item id="high" textValue="High">
+            High
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+          
+        </ListBox>
+      </Select.Popover>
+    </Select>
+
+                                <Select className="w-[256px]" name='Status' placeholder="Select one">
+                                <Label>Status</Label>
+                                <Select.Trigger>
+                                    <Select.Value />
+                                    <Select.Indicator />
+                                </Select.Trigger>
+                                <Select.Popover>
+                                    <ListBox>
+                                    <ListBox.Item id="pending" textValue="Pending">
+                                        Pending
+                                        <ListBox.ItemIndicator />
+                                    </ListBox.Item>
+                                    <ListBox.Item id="process" textValue="Process">
+                                        Process
+                                        <ListBox.ItemIndicator />
+                                    </ListBox.Item>
+                                    <ListBox.Item id="completed" textValue="Completed">
+                                        Completed
+                                        <ListBox.ItemIndicator />
+                                    </ListBox.Item>
+                                    
+                                    </ListBox>
+                                </Select.Popover>
+                                </Select>
+                  
+
+                  <TextField className="w-full" name="AssignTo">
+                    <Label>Assign To</Label>
+                    <Input placeholder="Enter your Assign To" />
                   </TextField>
 
                     <Modal.Footer>
